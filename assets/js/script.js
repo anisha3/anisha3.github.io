fb0d4a4495c482e1da788fc6904f1ac9798c1ac8
Anisha Salunkhe <anisha@berkeley.edu>
2016-04-26 18:11:44 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+
+  // QUESTION 6
+
+  //Implement the showing and hiding of the sidebar when the user clicks on #sidebar-button here:
+  
+$("#sidebar-button").click(function(){
+  if ($(".sidebar-container").hasClass("sidebar-active")) {
+      $("body").removeClass("no-scroll");
+      $("#sidebar-button").removeClass("button-active");
+      $(".sidebar-container").removeClass("sidebar-active");
+      $(".page-wrapper").removeClass("wrapper-active");
+  }
+  else {
+      $("#sidebar-button").addClass("button-active");
+      $(".sidebar-container").addClass("sidebar-active");
+      $(".page-wrapper").addClass("wrapper-active");
+      setTimeout(function() {
+        $("body").addClass("no-scroll");
+      }, 300);
+  }
+})
+
+  // QUESTION 7
+
+  //Implement the hiding of the sidebar when the user clicks on the page wrapper here:
+$(".page-wrapper").click(function(){
+  if ($(".sidebar-container").hasClass("sidebar-active")) {
+      $("body").removeClass("no-scroll");
+      $("#sidebar-button").removeClass("button-active");
+      $(".sidebar-container").removeClass("sidebar-active");
+      $(".page-wrapper").removeClass("wrapper-active");
+  }
+})
+
+  // QUESTION 8
+
+  //Implement the "slide to left" when the user clicks on #carousel-next here
+
+  $('#carousel-next').click(function(){
+    var temp = parseInt($('#carousel').css('margin-left').replace("px", ""));
+    if (temp == -3840){
+      return false;
+    } else {
+      var y = temp - 960;
+      $("#carousel").css('margin-left', y);
+    }
+  })
+
+  //Implement the "slide to right" when the user clicks on #carousel-prev here
+ $('#carousel-prev').click(function(){
+    var temp = parseInt($('#carousel').css('margin-left').replace("px", ""));
+    if (temp == 0){
+      return false;
+    } else {
+      var y = temp + 960;
+      $("#carousel").css('margin-left', y);
+    }
+  })
+
+
+  //THIS IS NOT A REQUIRED QUESTION 
+  // EXTRA FOR EXPERTS 
+
+  // Implement a "smooth scroll" when the user clicks on the sidebar links here
+
+});
